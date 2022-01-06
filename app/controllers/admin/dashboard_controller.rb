363a8fc3ba8530fb46602f74dd059c29ed6c59f6
Morsal Niyaz <morsalniyaz@gmail.com>
2022-01-06 09:36:54 -0800

    Added Count for Number of Products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,6 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @products_count = Product.order(id: :desc).all.count
   end
 end
